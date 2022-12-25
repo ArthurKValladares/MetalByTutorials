3eb451f0749ba39bdc6b3f75beb5da1e8b4ef53a
ArthurKV <arthur@brainiumstudios.com>
2022-12-25 03:01:59 -0300

    shader stuff

---
+++ MetalByTutorials/MetalByTutorials/MetalByTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalByTutorialsApp.swift
+//  MetalByTutorials
+//
+//  Created by Arthur Valladares on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct MetalByTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
